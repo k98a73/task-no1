2186d918d3af379d734f9c9c89ac2b480c894a18
k98a73 <100816862+k98a73@users.noreply.github.com>
2022-08-21 17:19:00 +0900

    testを３つ追加

--- knapsack.test.js
+++ knapsack.test.js
@@ -22,4 +22,46 @@ describe("knapsack", () => {
     ];
     expect(knapsack(items, 4)).toBe(6);
   });
+  test("knapsack3", () => {
+    // weight, value
+    const items = [
+      [1, 1],
+      [2, 2],
+      [3, 3],
+      [4, 4],
+      [5, 5],
+      [6, 6],
+      [7, 7],
+      [8, 8],
+      [9, 9],
+    ];
+    expect(knapsack(items, 20)).toBe(20);
+  });
+  test("knapsack4", () => {
+    // weight, value
+    const items = [
+      [1, 10],
+      [2, 25],
+      [3, 33],
+      [4, 48],
+      [5, 52],
+      [6, 61],
+    ];
+    expect(knapsack(items, 50)).toBe(229);
+  });
+  test("knapsack5", () => {
+    // weight, value
+    const items = [
+      [19, 10],
+      [23, 25],
+      [31, 33],
+      [45, 48],
+      [57, 52],
+      [62, 61],
+      [79, 70],
+      [83, 55],
+      [91, 93],
+    ];
+    expect(knapsack(items, 550)).toBe(447);
+  });
 });
